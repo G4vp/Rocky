a50bc37d2eb368cf077138ecbc55b908379c4fc9
Gabriel Viera <gviera2442@gmail.com>
2022-12-24 15:30:30 -0400

    Jumping added

--- Scripts/Rocky.cs
+++ Scripts/Rocky.cs
@@ -3,19 +3,29 @@ using System;
 
 public class Rocky : KinematicBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
 
-    // Called when the node enters the scene tree for the first time.
+    private int Jump = -400;
+    private int Gravity = 1400;
+    private Vector2 _velocity = new Vector2();
+
+    bool Jumped = false;
     public override void _Ready()
     {
-        
-    }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    }
+    public override void _PhysicsProcess(float delta)
+    {
+        GetInput();
+        _velocity.y += Gravity * delta;
+        if(Jumped && IsOnFloor()){
+            _velocity.y += Jump;
+            Jumped = false;
+        }
+        _velocity = MoveAndSlide(_velocity,Vector2.Up);
+    }
+    public void GetInput(){
+        if(Input.IsActionPressed("jump") && IsOnFloor()){
+            Jumped = true;           
+        }
+    }
 }
