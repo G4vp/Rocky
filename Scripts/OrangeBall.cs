e37cf283b603d3676642ec6bd56339486336dd26
Gabriel Viera <gviera2442@gmail.com>
2022-12-26 23:30:22 -0400

    SpawnRandomPosition function added to the OrangeBall class

--- Scripts/OrangeBall.cs
+++ Scripts/OrangeBall.cs
@@ -6,10 +6,15 @@ public class OrangeBall : KinematicBody2D
     private int speed = 50;
     private Vector2 _velocity = new Vector2();
 
+
+    
     public override void _PhysicsProcess(float delta)
     {   
         _velocity.x = -(speed * delta);
         MoveAndCollide(_velocity);
     }
 
+    public void SpawnRandomPosition(){
+
+    }
 }
