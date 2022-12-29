91e90741e2e444c1e8ad319e2e1e98a85ff03faa
Gabriel Viera <gviera2442@gmail.com>
2022-12-29 15:03:01 -0400

    Score system added

--- Scripts/ScoreSystem.cs
+++ Scripts/ScoreSystem.cs
@@ -3,15 +3,22 @@ using System;
 
 public class ScoreSystem : Node2D
 {
-    
+    Player PlayerNode;
+    Label LabelNode;
     public override void _Ready()
     {
-        
+        PlayerNode = GetParent().GetNode<Player>("Player");
+        LabelNode = GetNode<Label>("Label");
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+ // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        ScoreLabelUpdate();
+        GD.Print(PlayerNode.PlayerScore);
+    }
+
+    public void ScoreLabelUpdate(){
+        LabelNode.Text = "Score: " + PlayerNode.PlayerScore.ToString();
+    }
 }
