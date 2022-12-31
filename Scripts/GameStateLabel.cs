81557bb13ba0b550ed0e148bc44856ec11fb94e5
Gabriel Viera <gviera2442@gmail.com>
2022-12-31 03:18:58 -0400

    Label Changes

--- Scripts/GameStateLabel.cs
+++ Scripts/GameStateLabel.cs
@@ -4,6 +4,23 @@ using System;
 public class GameStateLabel : Node2D
 {
 
+    Label LabelNode;
+    AnimationPlayer AnimationLabelNode;
+    Node2D LabelParentNode;
+    public override void _Ready()
+    {
+        LabelParentNode = GetNode<Node2D>("LabelParent");
+        LabelNode = GetNode<Label>("LabelParent/Label");
+        AnimationLabelNode = GetNode<AnimationPlayer>("LabelParent/AnimationPlayer");
+    }
 
+    public void LabelToRestart(){
+        LabelNode.Text = "Press \"Space\" To Restart";
+        AnimationLabelNode.Play("HeartBeat");
+        Position = new Vector2 (Position.x,5);
+    }
 
+    public void HideRestartLabel(){
+
+    }
 }
