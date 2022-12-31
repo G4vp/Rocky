e83b2b946d740d8b8e63e49f41cbe0651fa6d36c
Gabriel Viera <gviera2442@gmail.com>
2022-12-30 23:57:09 -0400

    Score update animated

--- Scripts/ScoreLabel.cs
+++ Scripts/ScoreLabel.cs
@@ -5,12 +5,19 @@ public class ScoreLabel : Node2D
 {
     Player PlayerNode;
     Label LabelNode;
+    AnimationPlayer AnimationScoreLabel;
+    AnimationPlayer AnimationLabelParent;
+
+    int LastScore = 0;
     public override void _Ready()
     {
         PlayerNode = GetParent().GetNode<Player>("Player");
         LabelNode = GetNode<Label>("LabelParent/Label");
 
-        
+        AnimationScoreLabel = GetNode<AnimationPlayer>("AnimationPlayer");
+        AnimationLabelParent = GetNode<AnimationPlayer>("LabelParent/AnimationPlayer");
+
+
     }
 
  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,6 +27,10 @@ public class ScoreLabel : Node2D
     }
 
     public void ScoreLabelUpdate(){
-        LabelNode.Text = "Score: " + PlayerNode.PlayerScore.ToString();
+        if(PlayerNode.PlayerScore > LastScore){
+            AnimationScoreLabel.Play("UpdateScore");
+            LabelNode.Text = "Score: " + PlayerNode.PlayerScore.ToString();
+            LastScore = PlayerNode.PlayerScore;
+        }
     }
 }
