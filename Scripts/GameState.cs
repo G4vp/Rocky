c58e466ee3c4517d1e112f0fc17a87016ed4e13c
Gabriel <gviera2442@gmail.com>
2023-01-04 23:23:24 -0400

    Cleaning repo

--- Scripts/GameState.cs
+++ Scripts/GameState.cs
@@ -1,68 +1,68 @@
-    using Godot;
+	using Godot;
 using System;
 
 public class GameState : Node2D
 {   
-    public Player PlayerNode;
-    public EntitySpawner EntitySpawnerNode;
-    public Environment EnvironmentNode;
-    public ScoreLabel ScoreLabelNode;
+	public Player PlayerNode;
+	public EntitySpawner EntitySpawnerNode;
+	public Environment EnvironmentNode;
+	public ScoreLabel ScoreLabelNode;
 
-    public GameStateLabel GameStateLabelNode;
+	public GameStateLabel GameStateLabelNode;
 
-    bool isGameRunning = false;
-    bool isGameStarted = false;
-    
-    public override void _Ready()
-    {
-        PlayerNode = GetNode<Player>("Player");
-        EntitySpawnerNode = GetNode<EntitySpawner>("EntitySpawner");
-        EnvironmentNode = GetNode<Environment>("Environment");
-        ScoreLabelNode = GetNode<ScoreLabel>("ScoreLabel");
-        GameStateLabelNode = GetNode<GameStateLabel>("GameStateLabel");
-        
-        PlayerNode.StopPlayer();
-        EnvironmentNode.StopEnvironment();
-    }
+	bool isGameRunning = false;
+	bool isGameStarted = false;
+	
+	public override void _Ready()
+	{
+		PlayerNode = GetNode<Player>("Player");
+		EntitySpawnerNode = GetNode<EntitySpawner>("EntitySpawner");
+		EnvironmentNode = GetNode<Environment>("Environment");
+		ScoreLabelNode = GetNode<ScoreLabel>("ScoreLabel");
+		GameStateLabelNode = GetNode<GameStateLabel>("GameStateLabel");
+		
+		PlayerNode.StopPlayer();
+		EnvironmentNode.StopEnvironment();
+	}
 
-    public override void _Process(float delta)
-    {   
-        GetInput();
-        
-        if(isGameStarted){
-            GameStarted();
-        }
-        if(PlayerNode.IsGameOver){
-            GameOver();
-        }
-    }
-    public void GameOver(){
-        PlayerNode.StopPlayer();
-        EntitySpawnerNode.StopSpawn();
-        ScoreLabelNode.ScoreLabelGameOver();
-        GameStateLabelNode.LabelToRestart();
+	public override void _Process(float delta)
+	{   
+		GetInput();
+		
+		if(isGameStarted){
+			GameStarted();
+		}
+		if(PlayerNode.IsGameOver){
+			GameOver();
+		}
+	}
+	public void GameOver(){
+		PlayerNode.StopPlayer();
+		EntitySpawnerNode.StopSpawn();
+		ScoreLabelNode.ScoreLabelGameOver();
+		GameStateLabelNode.LabelToRestart();
 
 
-        isGameRunning = false;
-        isGameStarted = false;
-    }
+		isGameRunning = false;
+		isGameStarted = false;
+	}
 
-    public void GameStarted(){
-        ScoreLabelNode.Show();
-        GameStateLabelNode.HideLabel();
-        
-        PlayerNode.ResetPlayer();
-        EntitySpawnerNode.ResetSpawn();
-        ScoreLabelNode.ResetScoreLabel();
-        EnvironmentNode.ResetEnvironment();
-        isGameStarted = false;
+	public void GameStarted(){
+		ScoreLabelNode.Show();
+		GameStateLabelNode.HideLabel();
+		
+		PlayerNode.ResetPlayer();
+		EntitySpawnerNode.ResetSpawn();
+		ScoreLabelNode.ResetScoreLabel();
+		EnvironmentNode.ResetEnvironment();
+		isGameStarted = false;
 
-    }
+	}
 
-    public void GetInput(){
-        if(Input.IsActionJustPressed("jump") && !isGameRunning ){
-            isGameStarted = true;
-            isGameRunning = true;
-        }
-    }
+	public void GetInput(){
+		if(Input.IsActionJustPressed("jump") && !isGameRunning ){
+			isGameStarted = true;
+			isGameRunning = true;
+		}
+	}
 }
